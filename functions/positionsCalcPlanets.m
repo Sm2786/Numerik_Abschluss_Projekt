9d3269682df09055d366a0a4ac2e71251d4f4f82
corneliusbrandt <bc7970@mci4me.at>
2025-01-08 15:24:33 +0100

    rename positionsCalcPlanets

--- functions/positionsCalcPlanets.m
+++ functions/positionsCalcPlanets.m
@@ -1,4 +1,4 @@
-function [position_earth,position_mars] = positionCalcPlanets(constants, time)
+function [position_earth,position_mars] = positionsCalcPlanets(constants, time)
 %Function to calculate the position of earth and mars at a wanted time
 earthRadius = constants.earthOrbitRadius;
 marsRadius = constants.marsOrbitRadius;
@@ -19,7 +19,7 @@ position_earth(2, 1) = earthRadius * sin(theta);
 
 theta = marsOmega * time;
 position_mars(1, 1) = marsRadius * cos(theta);
-position_mars(2, 1) = marsRadius * sin(theta);
+position_mars(2, 1) = marsRadius * sin(theta);S
 
 position_earth
 position_mars
